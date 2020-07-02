4c7a01b2c09e18c1d51b603f2c553c1238e89649
KoSangWon <jackoss@naver.com>
2020-07-02 17:00:26 +0900

    movie api using graphql

--- graphql/db.js
+++ graphql/db.js
@@ -1,50 +1,22 @@
-export let movies = [
-    {
-        id: 0,
-        name: "Star Wars - The new one",
-        score: 1
-    },
-    {
-        id: 1,
-        name: "Avengers - The new one",
-        score: 8
-    },
-    {
-        id: 2,
-        name: "The Godfather",
-        score: 99
-    },
-    {
-        id: 3,
-        name: "Logan",
-        score: 2
+import fetch from "node-fetch";
+const API_URL = "https://yts.mx/api/v2/list_movies.json?"
+
+//데이터 확인
+// export const getMovies = (limit, rating) => fetch(`${API_URL}`)
+//     .then(res => res.json())
+//     .then(json => json.data.movies);//{}로 안감싸면 return임
+
+//
+export const getMovies = (limit, rating) => {
+    let REQUEST_URL = API_URL;
+    if(limit > 0){
+        REQUEST_URL += `limit=${limit}`;
     }
-];
-
-export const getMovies = () => movies;
-
-export const getById = id => {
-    const filteredMovies = movies.filter(movie => movie.id === id);
-    return filteredMovies[0]
+    if(rating > 0){
+        REQUEST_URL += `&minimum_rating=${rating}`;
+    }
+    return fetch(REQUEST_URL)
+        .then(res => res.json())
+        .then(json => json.data.movies);
 };
-
-export const deleteMovie = id => {
-    const cleanedMovies = movies.filter(movie => movie.id !== id);
-    if(movies.length > cleanedMovies.length){
-        movies = cleanedMovies;
-        return true;
-    } else {
-        return false;
-    }
-};
-
-export const addMovie = (name, score) => {
-    const newMovie = {
-        id: `${movies.length + 1}`,
-        name,
-        score
-    }
-    movies.push(newMovie);
-    return newMovie;
-}
 
